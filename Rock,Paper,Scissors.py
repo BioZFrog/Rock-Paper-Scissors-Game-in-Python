6b7bc4aad28383999ad4bde32c73c41ca98cec74
BioZFrog <133135387+BioZFrog@users.noreply.github.com>
2024-04-16 10:06:51 +0300

    Game.py

---
+++ Rock,Paper,Scissors.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+Done = False
+wins, falses, ties = 0, 0, 0 
+
+print("--------___------------------Rock, Paper, Scissors Game in Python------------------___--------")
+time.sleep(3)
+while not Done:
+    choice = input("\nEnter a choice:  ")
+    computer_choice = random.choice(['Rock','Paper', 'Scissors'])
+
+    if choice == computer_choice:
+        print(f"Its a tie! Both Chose {choice}\n")
+        Done = True
+
+    elif choice == 'Rock':
+        if computer_choice == 'Paper':
+            print(f"Computer Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+        else:
+            print(f"You Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+    elif choice == 'Paper':
+        if computer_choice == 'Scissors':
+            print(f"Computer Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+        else:
+            print(f"You Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+    elif choice == 'Scissors':
+        if computer_choice == 'Rock':
+            print(f"Computer Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+        else:
+            print(f"You Won! You chose {choice} and Computer chose {computer_choice}\n")
+            Done = True
+    else:
+        print("Please enter a valid Choice\n")
+        Done = True
